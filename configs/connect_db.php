d22eeb52efc2d306104b65d9a79731a91a9ba4a0
nathakritbc <std60010912540@acc.msu.ac.th>
2022-05-10 16:05:18 +0700

    update port connect

--- configs/connect_db.php
+++ configs/connect_db.php
@@ -3,10 +3,11 @@ $servername = "localhost";
 $username = "sa";
 $password = "sa";
 $dbname = "ltax";
+$db_port="3304";
 
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname,$db_port);
 // Change character set to utf8
 mysqli_set_charset($conn,"utf8");
 // Check connection
