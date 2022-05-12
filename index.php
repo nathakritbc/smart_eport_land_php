3df2d018784dbcca24819c889ef7b921d73d38c3
nathakritbc <std60010912540@acc.msu.ac.th>
2022-05-12 14:27:59 +0700

    ad

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
  <?php
 /* This will give an error. Note the output
  * above, which is before the header() call */
-header('Location: pages/update_owner_land.php');
+header('Location: pages/index.php');
 exit;
 ?>
