3ebd5ff2553f96a98ccb9ab61d1ecb5ed974666a
nathakritbc <std60010912540@acc.msu.ac.th>
2022-05-12 15:56:05 +0700

    add

--- pages/layout/sidebar.php
+++ pages/layout/sidebar.php
@@ -118,7 +118,7 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="" class="nav-link">
+                            <a href="../pages/building_list.php" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>รายการสิ่งปลูกสร้าง</p>
                             </a>
