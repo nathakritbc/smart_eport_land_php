13ff8aa3013fd842dc3c2444bf01441c162ba340
nathakritbc <std60010912540@acc.msu.ac.th>
2022-05-12 16:27:39 +0700

    ad

--- pages/building_list.php
+++ pages/building_list.php
@@ -81,6 +81,44 @@ $sql = "SELECT b.bid,b.b_code,b.parcel_code,b.b_type,b.b_material,b.build_year,b
 
                                     </div>
 
+                                    <div class="row">
+                                        <div class="form-inline">
+                                            <div class="input-group" data-widget="sidebar-search">
+                                                <input class="form-control form-control-sidebar" type="search"
+                                                    placeholder="Search" aria-label="Search">
+                                                <div class="input-group-append">
+                                                    <button class="btn btn-sidebar">
+                                                        <i class="fas fa-search fa-fw"></i>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                            <div class="sidebar-search-results">
+                                                <div class="list-group"><a href="#" class="list-group-item">
+                                                        <div class="search-title"><strong
+                                                                class="text-light"></strong>N<strong
+                                                                class="text-light"></strong>o<strong
+                                                                class="text-light"></strong> <strong
+                                                                class="text-light"></strong>e<strong
+                                                                class="text-light"></strong>l<strong
+                                                                class="text-light"></strong>e<strong
+                                                                class="text-light"></strong>m<strong
+                                                                class="text-light"></strong>e<strong
+                                                                class="text-light"></strong>n<strong
+                                                                class="text-light"></strong>t<strong
+                                                                class="text-light"></strong> <strong
+                                                                class="text-light"></strong>f<strong
+                                                                class="text-light"></strong>o<strong
+                                                                class="text-light"></strong>u<strong
+                                                                class="text-light"></strong>n<strong
+                                                                class="text-light"></strong>d<strong
+                                                                class="text-light"></strong>!<strong
+                                                                class="text-light"></strong></div>
+                                                        <div class="search-path"></div>
+                                                    </a></div>
+                                            </div>
+                                        </div>
+                                    </div>
+
                                     <!-- <div class="row mt-3">
                                         <form @submit.prevent="submitFormReport">
                                             <div class="form-group">
